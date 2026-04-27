dc2c0394bc6b05c4e502fecc3d72fe6f3e02d0ab
Monkar44 <m.kubas2009@gmail.com>
2026-04-27 21:15:44 +0200

    add TextFields

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,9 +1,13 @@
 package com.example.wokolskidashboard.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -25,6 +30,24 @@ fun IncomeForm(onIncomeAdded: (String, Double) -> Unit) {
             .padding(16.dp)
     ){
         Text("Księgowanie przychodów (Rzecki)", style = MaterialTheme.typography.headlineSmall)
+
+        OutlinedTextField(
+            value = title,
+            onValueChange = { title = it },
+            label = { Text("Towar (np. Paryskie rękawiczki)") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = amountString,
+            onValueChange = { amountString = it },
+            label = { Text("Kwota w rublach") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
     }
 
 }
