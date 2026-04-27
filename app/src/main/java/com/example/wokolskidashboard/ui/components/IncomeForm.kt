24d4cdd3e2825772fc665f225b15ae07d45792f7
Monkar44 <m.kubas2009@gmail.com>
2026-04-27 21:17:45 +0200

    create button

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -48,6 +50,19 @@ fun IncomeForm(onIncomeAdded: (String, Double) -> Unit) {
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
         )
         Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                val amount = amountString.toDoubleOrNull() ?: 0.0
+                onIncomeAdded(title, amount)
+                title = ""
+                amountString = ""
+            },
+            enabled = isValid,
+            modifier = Modifier.align(Alignment.End)
+        ){
+            Text("Dodaj Zysk")
+        }
     }
 
 }
