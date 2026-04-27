abb1aa8cf02436bcbe32a7abe1b6734a593e7b69
Monkar44 <m.kubas2009@gmail.com>
2026-04-27 21:00:38 +0200

    create function IncomeForm

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,2 +1,16 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun IncomeForm(onIncomeAdded: (String, Double) -> Unit) {
+    var title by remember { mutableStateOf("") }
+    var amountString by remember { mutableStateOf("") }
+    val isValid = title.isNotBlank() && (amountString.toDoubleOrNull() ?: 0.0) > 0.0
+
+}
+
