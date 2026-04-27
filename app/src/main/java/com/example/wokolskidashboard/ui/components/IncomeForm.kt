c1c9e876c89075f8be7026fbd0a87f6f0d74cf2e
Monkar44 <m.kubas2009@gmail.com>
2026-04-27 21:05:27 +0200

    style and text

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,16 +1,31 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun IncomeForm(onIncomeAdded: (String, Double) -> Unit) {
     var title by remember { mutableStateOf("") }
     var amountString by remember { mutableStateOf("") }
     val isValid = title.isNotBlank() && (amountString.toDoubleOrNull() ?: 0.0) > 0.0
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ){
+        Text("Księgowanie przychodów (Rzecki)", style = MaterialTheme.typography.headlineSmall)
+    }
 
 }
 
